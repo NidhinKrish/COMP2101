a7bfba98affa09b7539437614d119c21bd598f28
NidhinKrish <71674951+NidhinKrish@users.noreply.github.com>
2020-12-17 13:52:10 -0800

    Lab 5
    Lab 5

---
+++ Powershell/lab5.ps1
@@ -0,0 +1,41 @@
+﻿param ( [switch]$System, 
+        [switch]$Disks,
+        [switch]$Network
+)
+
+if ($System -eq $false -and $Disks -eq $false -and $Network -eq $false) {
+        "SYSTEM INFO "
+        "HARDWARE INFO :  "
+        hardware_info | fl
+        "OS INFO : "
+        os_info | fl
+        "CPU INFO : "
+        processor_info | fl
+        "RAM INFO : "
+        memory_info| ft -AutoSize
+        "DISK INFO : "
+        disk_info | ft -AutoSize
+        "NET INFO: "
+        net_info | fl
+    } 
+else { 
+    if ($System) {
+        "HARDWARE INFO : "
+        hardware_info | fl
+        "OS INFO : "
+        os_info | fl
+        "CPU INFO : "
+        processor_info | fl
+        "RAM INFO : "
+        memory_info| ft -AutoSize
+    }
+    if ($Disks) {
+        "DISK INFO : "
+        disk_info | ft -AutoSize
+    }
+    if ($Network) {
+        "NETWORK INFO :"
+        net_info | fl
+    }
+
+}
